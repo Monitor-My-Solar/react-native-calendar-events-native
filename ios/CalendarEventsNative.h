227cf0616fe69cd5266f512254715d3e10951a03
Zakery <zakery@Zakerys-MacBook-Pro.local>
2025-09-11 20:51:14 +0100

    Bump

--- ios/CalendarEventsNative.h
+++ ios/CalendarEventsNative.h
@@ -1,5 +1,5 @@
 #ifdef RCT_NEW_ARCH_ENABLED
-#import "RNCalendarEventsNativeSpec.h"
+#import <RNCalendarEventsNativeSpec/RNCalendarEventsNativeSpec.h>
 @interface CalendarEventsNative : NSObject <NativeCalendarEventsNativeSpec>
 #else
 #import <React/RCTBridgeModule.h>
